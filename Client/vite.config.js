1dd1c6c7a652b9e2233061f32eb7bdab38dd5c32
Thabo Jimmy Browen <142535245+Tjbrowen@users.noreply.github.com>
2024-04-30 19:30:49 +0200

    modified:   Client/vite.config.js

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,1 +1,15 @@
+import { defineConfig } from 'vite'; // Import defineConfig from vite
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    // Other build options...
+    rollupOptions: {
+      // Other Rollup options...
+      external: ['express-async-handler']
+    }
+  }
+});
 
